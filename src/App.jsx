57f3da0b74e25cb7aedb09ad29ac85aeb508f209
df-Ansh <168733290+df-Ansh@users.noreply.github.com>
2025-07-17 19:46:50 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline text-red-500">
+      Application is running!
+    </div>
+  )
+}
+
+export default App
